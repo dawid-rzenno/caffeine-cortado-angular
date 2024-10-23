4753d0559165cb18bf6f62e7bd8860d4bfc9dfb7
Dawid Rzenno <dawid.rzenno@protonmail.com>
2024-10-23 18:49:00 +0200

    WIP

--- src/app/workout/workout.service.ts
+++ src/app/workout/workout.service.ts
@@ -1,33 +1,16 @@
 import { Injectable } from '@angular/core';
 import { environment } from "../../environments/environment";
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { map, Observable } from "rxjs";
-import { PaginatedResponse } from "../shared/models/paginated-response";
+import { HttpClient } from "@angular/common/http";
 import { ItemBase } from "../shared/models/item-base";
-import { PaginationParams } from "../shared/models/mat-paginator-config";
+import { ItemServiceAbstract } from "../shared/abstracts/item-service.abstract";
 
 @Injectable({
   providedIn: 'root'
 })
-export class WorkoutService {
+export class WorkoutService extends ItemServiceAbstract<ItemBase, ItemBase> {
+  protected readonly endpointUrl: string = `${environment.apiUrl}/fitness/workout`;
 
-  private readonly endpointUrl: string = `${environment.apiUrl}/fitness/workout`;
-
-  constructor(private http: HttpClient) { }
-
-  create(): Observable<ItemBase> {
-    return this.http.post<ItemBase>(`${this.endpointUrl}`, undefined)
-  }
-
-  get(id: string): Observable<ItemBase> {
-    return this.http.get<ItemBase>(`${this.endpointUrl}/${id}`)
-  }
-
-  getAll(params?: Partial<PaginationParams>): Observable<PaginatedResponse<ItemBase>> {
-    return this.http.get<PaginatedResponse<ItemBase>>(`${this.endpointUrl}`, {
-      params: new HttpParams({ fromObject: params })
-    }).pipe(
-      map((paginatedResponse: PaginatedResponse<ItemBase>) => new PaginatedResponse<ItemBase>(paginatedResponse))
-    )
+  constructor(protected override http: HttpClient) {
+    super(http);
   }
 }
