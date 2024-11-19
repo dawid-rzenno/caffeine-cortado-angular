611835d6bcc8c14fd046b60d455dd9484abc84cb
Dawid Rzenno <dawid.rzenno@protonmail.com>
2024-11-19 17:49:17 +0100

    Create a component that will enable user to manage application settings.

---
+++ src/app/core/user/user-settings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "cortado-user",
+  standalone: true,
+  imports: [],
+  templateUrl: "./user-settings.component.html",
+  styleUrl: "./user-settings.component.scss",
+})
+export class UserSettingsComponent {}
