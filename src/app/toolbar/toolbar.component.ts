9ba96272e0f787b1424072578b83db195b6ac31d
Dawid Rzenno <dawid.rzenno@protonmail.com>
2025-07-20 23:20:17 +0200

    Display signed-in user.

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -3,19 +3,27 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from "@angular/material/icon";
 import { AppService } from "../app.service";
+import { AuthService } from "../auth/auth.service";
+import { User } from "../auth/user";
+import { BehaviorSubject } from "rxjs";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
 	selector: 'app-toolbar',
 	imports: [
 		MatToolbarModule,
 		MatButtonModule,
-		MatIconModule
+		MatIconModule,
+		AsyncPipe
 	],
 	templateUrl: './toolbar.component.html',
 	styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
-	constructor(private appService: AppService) {
+	readonly user$: BehaviorSubject<User | undefined>
+
+	constructor(private appService: AppService, private authService: AuthService) {
+		this.user$ = this.authService.user$;
 	}
 
 	openSideNav(): void {
